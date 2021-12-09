179b5c4eff2c129b04dbcd8889ebe92a8810e7d1
Paul Veillard <36556329+veilair@users.noreply.github.com>
2021-12-08 20:40:44 -0500

    Update latin_translator.py

--- python-scripts/latin_translator.py
+++ python-scripts/latin_translator.py
@@ -1,4 +1,12 @@
-# latin_translator.py
+# 1 - The python scripts in this example are written using Python 3.9 and the Python PyCharm IDE. To use it, you must first install the PyCharm IDE on your device.
+
+
+# 2 - To execute latin_translator.py from the terminal, type the following code. Example, From Your Terminal Run 'python3 latin_translator.py'
+
+# $ python3 latin_translator.py
+
+
+## latin_translator.py
 
 #request user for input
 user_input = input("Input text to translate to pig latin: ")
