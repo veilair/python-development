f4f63f2792692ae5e5e012d3b78ff76245c354c0
Paul Veillard <36556329+veilair@users.noreply.github.com>
2021-12-08 20:43:17 -0500

    Create join_strings.py

---
+++ python-scripts/join_strings.py
@@ -0,0 +1,19 @@
+# join_strings.py
+
+string1 = "my"
+string2 = "work"
+
+joined_string = string1 +string2
+
+print(joined_string)
+
+
+#-----------------------------------------------------------------------------------------------------
+
+# In Python, there are a variety of ways to join string values. This is called string concatenation.
+
+# The ‘+’ operator is the simplest way to combine two string values in Python.
+
+# To learn how to connect two strings, build a python script with the following script.
+
+# Two string values are allocated to two variables, with a third variable used to store the joined values, which will be printed later.
